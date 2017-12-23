58a2c929d602dbeac12e783f6cf461bb273fb73b
letusfly85 <letusfly95@gmail.com>
2017-12-23 16:30:44 +0900

    initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'schema_mapper_generator'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
